621fdda0ea11ad0e0ad42843ad9706311ea57532
hossein <hossein.mahdiyan@gmail.com>
2021-11-26 19:24:00 +0100

    - sql to add new data - expose id of jpa entities - get products by product category id

--- src/main/java/com/hossein/eshopweb/config/DataRestConfig.java
+++ src/main/java/com/hossein/eshopweb/config/DataRestConfig.java
@@ -2,14 +2,28 @@ package com.hossein.eshopweb.config;
 
 import com.hossein.eshopweb.entity.Product;
 import com.hossein.eshopweb.entity.ProductCategory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 @Configuration
 public class DataRestConfig implements RepositoryRestConfigurer {
+    private EntityManager entityManager;
+
+    @Autowired
+    public DataRestConfig(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
         RepositoryRestConfigurer.super.configureRepositoryRestConfiguration(config, cors);
@@ -24,5 +38,22 @@ public class DataRestConfig implements RepositoryRestConfigurer {
                 .forDomainType(ProductCategory.class)
                 .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(unSupportActions)))
                 .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(unSupportActions)));
+
+        //expose id of entities
+        exposeIds(config);
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+        List<Class> entityClasses = new ArrayList<>();
+
+        for (EntityType<?> entity : entities) {
+            entityClasses.add(entity.getJavaType());
+        }
+
+        Class[] classes = entityClasses.toArray(new Class[0]);
+
+        config.exposeIdsFor(classes);
     }
 }
--- src/main/java/com/hossein/eshopweb/dao/ProductRepository.java
+++ src/main/java/com/hossein/eshopweb/dao/ProductRepository.java
@@ -1,9 +1,14 @@
 package com.hossein.eshopweb.dao;
 
 import com.hossein.eshopweb.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @CrossOrigin
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    
+    Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
 }
