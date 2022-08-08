bbdb4d038b19d277d481b33de189392bc527ea4e
hossein <hossein.mahdiyan@gmail.com>
2022-08-08 08:02:50 +0200

    Cross origin controller

--- src/main/java/com/hossein/eshopweb/config/AppConfig.java
+++ src/main/java/com/hossein/eshopweb/config/AppConfig.java
@@ -3,9 +3,15 @@ package com.hossein.eshopweb.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/*
+This class meant to be used for a central  CrossOrigin handler for RestController but for what ever reason it is not working
+redundant as @CrossOrigin in RestController only works
+ */
 @Configuration
+@EnableWebMvc
 public class AppConfig implements WebMvcConfigurer {
 
     @Value("${spring.data.rest.base-path}")
@@ -16,7 +22,6 @@ public class AppConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry) {
-        WebMvcConfigurer.super.addCorsMappings(corsRegistry);
         corsRegistry.addMapping(basePath + "/**").allowedOrigins(theAllowedOrigins);
     }
 }
